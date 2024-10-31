47be6da11ffae39ed68b1f4ed72008d8b491510a
이지원 <wldnjs7851@naver.com>
2024-10-31 15:54:21 +0900

    feat: svg 파일 설정 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+  plugins: [react(), svgr(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
 
