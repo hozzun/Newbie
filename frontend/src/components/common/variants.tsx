b8bf5e997953b930d164aba40f25afb9f43679a0
이지원 <wldnjs7851@naver.com>
2024-10-31 13:13:51 +0900

    feat: 버튼 속성 추가

--- frontend/src/components/common/variants.tsx
+++ frontend/src/components/common/variants.tsx
@@ -1,4 +1,6 @@
 export enum BUTTON_VARIANTS {
   primary = "bg-green-900 text-white hover:bg-green-950",
+  second = "bg-gray-300 text-white hover:bg-gray-400",
   error = "bg-error-400 text-white hover:bg-error-500",
+  primaryText = "text-green-900 hover:bg-green-50",
 }
