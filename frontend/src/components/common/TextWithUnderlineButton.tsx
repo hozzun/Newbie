1484d8aad09d93d70d74fada00c71bd8fd8e1864
이지원 <wldnjs7851@naver.com>
2024-10-31 17:06:39 +0900

    refactor: 밑줄 포함된 버튼 컴포넌트 이름 변경

--- frontend/src/components/common/TextWithUnderlineButton.tsx
+++ frontend/src/components/common/TextWithUnderlineButton.tsx
@@ -1,12 +1,12 @@
-type TextButtonWithUnderlineProps = {
+type TextWithUnderlineButtonProps = {
   className?: string;
   children: React.ReactNode;
 };
 
-const TextButtonWithUnderline = (props: TextButtonWithUnderlineProps) => {
+const TextWithUnderlineButton = (props: TextWithUnderlineButtonProps) => {
   const textButtonWithUnderlineClass = `${props.className || ""} px-2 py-0.5 text-xs rounded-lg hover:bg-gray-50 font-kbogothiclight underline`;
 
   return <button className={textButtonWithUnderlineClass}>{props.children}</button>;
 };
 
-export default TextButtonWithUnderline;
+export default TextWithUnderlineButton;
