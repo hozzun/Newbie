94c5985cc8db747ae80bbe23c091b6d5a63b991b
jinwoo <yasisicoco@gmail.com>
2024-10-31 13:13:52 +0900

    feat: 구단셀렉 캐러셀버전 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import ClubSelect from "./components/common/ClubSelect";
+import ClubSelectItem from "./components/common/ClubSelectItem";
 
 function App() {
   return (
     <>
       <p className="text-green-900 font-kbogothicbold">Hello World</p>
-      <div><ClubSelect /></div>
+      <div>
+        <ClubSelectItem />
+      </div>
     </>
   );
 }
