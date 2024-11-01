667149c4caec48800fba59eaa41424306f78b00b
김미량 <miryang@naver.com>
2024-11-01 14:47:17 +0900

    refactor: add alarm icon

--- frontend/src/components/common/AppBar.tsx
+++ frontend/src/components/common/AppBar.tsx
@@ -1,14 +1,18 @@
 import Logo from '../../assets/images/LOGO.png'
+import Bell from "../../assets/icons/bell.svg?react"
 
 const AppBar = () => {
 
+  const onClick = () => {}
+
   return (
-    <div className="flex items-center bg-white font-kbogothiclight border-b border-l border-r border-gray-200 rounded-b-2xl w-full h-16">
+    <div className="flex justify-between items-center bg-white font-kbogothiclight border-b border-l border-r border-gray-200 rounded-b-2xl w-full h-16">
     <img
         src={Logo}
         alt="로고"
         className='ml-3 mt-3 w-14 h-14'
       />
+      <Bell className="w-6 h-6 text-green-900 mr-3 mt-3" onClick={onClick} />
     </div>
   );
 };
