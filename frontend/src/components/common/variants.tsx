a14cc047637b2844e1e8e572612da2bd90e919f7
이지원 <wldnjs7851@naver.com>
2024-11-01 10:40:17 +0900

    feat: 버튼 종류 추가

--- frontend/src/components/common/variants.tsx
+++ frontend/src/components/common/variants.tsx
@@ -1,8 +1,9 @@
 export enum BUTTON_VARIANTS {
-  primary = "bg-green-900 text-white hover:bg-green-950",
-  second = "bg-gray-300 text-white hover:bg-gray-400",
-  error = "bg-error-400 text-white hover:bg-error-500",
-  primaryText = "text-green-900 hover:bg-green-50",
+  primary = "py-4 bg-green-900 text-white rounded-lg hover:bg-green-950",
+  second = "py-4 bg-gray-300 text-white rounded-lg hover:bg-gray-400",
+  error = "py-4 bg-error-400 text-white rounded-lg hover:bg-error-500",
+  primaryText = "py-4 text-green-900 rounded-lg hover:bg-green-50",
+  white = "py-1 text-white border border-white rounded-2xl hover:bg-white/10",
 }
 
 export enum FLOATING_BUTTON_VARIANTS {
