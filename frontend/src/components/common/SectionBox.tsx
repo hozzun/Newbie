7227c6c45e6cee4a74c9d281968897c3395991a7
김미량 <miryang@naver.com>
2024-11-01 15:19:50 +0900

    refactor: add left angle icon

--- frontend/src/components/common/SectionBox.tsx
+++ frontend/src/components/common/SectionBox.tsx
@@ -1,11 +1,18 @@
+import AngleLeft from "../../assets/icons/angle-left.svg?react"
+
 interface SectionBoxProps {
   label?: string;
 }
 
 const SectionBox = ({ label }: SectionBoxProps) => {
 
+  const onClick = () => {console.log('클릭')}
+
   return (
-    <div className="flex items-center justify-center bg-white text-gray-700 font-kbogothicmedium w-full h-14">{label}</div>
+    <div className="flex items-center bg-white text-gray-700 font-kbogothicmedium w-full h-14">
+      <AngleLeft className="w-5 h-5 text-gray-700 ml-3" onClick={onClick} />
+      <label className="flex-grow text-center mr-3">{label}</label>
+    </div>
   );
 };
 
