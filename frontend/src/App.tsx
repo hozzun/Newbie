aaa68e70f73edf6ac56286ae5c11383d122b606f
김미량 <miryang@naver.com>
2024-11-01 15:29:24 +0900

    feat: 공통 컴포넌트 생성 완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import SelectBox from "./components/common/SelectBox";
-
 function App() {
 
   return (
     <>
       <p className="text-green-900 font-kbogothicbold">Hello World</p>
-      <SelectBox label="주소" />
     </>
   );
 }
