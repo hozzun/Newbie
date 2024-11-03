57ce4763972531b6b52cffd32852dcb3aee409ba
김호준 <didl95@naver.com>
2024-11-04 01:21:20 +0900

    Revert "Merge branch 'feature/9-baseball-save' into 'master'"
    This reverts merge request !26

--- backend/baseball/src/main/java/com/newbie/baseball/domain/player/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.newbie.baseball.domain.player.repository;
-
-import com.newbie.baseball.domain.player.entity.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
-}
--- backend/baseball/src/main/java/com/newbie/baseball/domain/player/service/PlayerService.java
+++
@@ -1,8 +0,0 @@
-package com.newbie.baseball.domain.player.service;
-
-import com.newbie.baseball.domain.player.dto.res.PlayerResponseDto;
-
-public interface PlayerService {
-
-    PlayerResponseDto getPlayerById(Integer id);
-}
