49f48e556e74054e0e8cd93db6965445c8209e6f
김미량 <miryang@naver.com>
2024-11-04 14:23:58 +0900

    refactor: css 스타일 수정

---
+++ frontend/src/components/cheerteam/CheerTeam.tsx
@@ -0,0 +1,11 @@
+const CheerTeam = () => {
+
+  return (
+    <div className="flex flex-col ml-5 justify-center">
+      <label className="font-kbogothicbold text-2xl text-gray-600 m-3">어떤 팀을 응원하시나요?</label>
+      <label className="flex justify-center items-center font-kbogothicbold w-11/12 h-10 m-3 mb-5 text-green-900 rounded-full border border-2 border-green-300">나에게 맞는 팀 찾기</label>
+    </div>
+  )
+}
+
+export default CheerTeam
