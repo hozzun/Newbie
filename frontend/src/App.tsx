556d10451294506d3f9e8f444f438edbdff5f79e
jinwoo <yasisicoco@gmail.com>
2024-11-05 01:55:24 +0900

    feat: App.tsx에 router 등록 및 axios 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import Login from "../src/pages/auth/Login";
-import Redirect from "../src/pages/auth/Redirect";
+import AppRouter from "./AppRouter";
 
 function App() {
   return (
     <>
       <p className="text-green-900 font-kbogothicbold">Hello World</p>
-      <Redirect />
-      <Login />
+      <AppRouter />
     </>
   );
 }
