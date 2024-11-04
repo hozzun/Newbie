a63f2bdc7115f8f4f1cba521b9b29cfb0079d6d7
김미량 <miryang@naver.com>
2024-11-04 22:54:41 +0900

    feat: 나의 직관 경기 상세 조회 페이지 구현 완료

--- frontend/src/components/mypage/GameResult.tsx
+++ frontend/src/components/mypage/GameResult.tsx
@@ -1,17 +1,33 @@
+import ClubSelectItem from "../common/ClubSelectItem";
+import ClubLogos from "../../util/ClubLogos";
+
 interface GameResultProps {
   team1: string;
   team2: string;
-  time: string;
-  loc: string;
+  score1: number;
+  score2: number;
 }
 
 const GameResult = (props: GameResultProps) => {
   return (
-    <div className="flex justify-center items-center font-kbogothicmedium">
-      <div className="m-3">
-        <label className="mx-2 text-xs text-gray-600">경기 전</label>
-        <label className="mx-2 text-gray-600">{props.time}</label>
-        <label className="mx-2 text-gray-300">{props.loc}</label>
+    <div className="font-kbogothicmedium">
+      <div className="flex flex-row justify-center items-center m-3">
+        <ClubSelectItem
+          clubColor="samsung" 
+          logo={ClubLogos.samsung} 
+          isSelected={false}
+        />
+        <label className="m-3 mr-10">{props.team1}</label>
+        <label className="m-3 ml-20">{props.score1}</label>
+      </div>
+      <div className="flex flex-row justify-center items-center m-3">
+        <ClubSelectItem
+          clubColor="ssg" 
+          logo={ClubLogos.ssg}
+          isSelected={false}
+        />
+        <label className="m-3 mr-10">{props.team2}</label>
+        <label className="m-3 ml-20">{props.score2}</label>
       </div>
     </div>
   );
