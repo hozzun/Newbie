c651f6a120bfe3b732ec3a4860d1b71b9911118f
김미량 <miryang@naver.com>
2024-11-05 21:32:39 +0900

    feat: 나의 직관 경기 등록 컴포넌트 및 페이지 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,7 @@
-// import AppRouter from "./AppRouter";
-import ModalForm from "./pages/mypage/ModalForm"
-import WatchGame from "./pages/mypage/WatchGame";
-
 function App() {
   return (
     <>
-      {/* <p className="text-green-900 font-kbogothicbold">Hello World</p> */}
-      {/* <AppRouter /> */}
-      {/* <ModalForm /> */}
-      <WatchGame />
+      <p className="text-green-900 font-kbogothicbold">Hello World</p>
     </>
   );
 }
--- frontend/src/components/mypage/ModalForm.tsx
+++ frontend/src/components/mypage/ModalForm.tsx
@@ -2,11 +2,9 @@ interface ModalFormProps {
   date: string;
   team1: string;
   team2: string;
-  isOpen: boolean; // 모달 열림 상태
 }
 
 const ModalForm = (props: ModalFormProps) => {
-  if (!props.isOpen) return null;
 
   return (
     <div className="flex flex-col items-center justify-center font-kbogothicbold mt-10 mb-5">
