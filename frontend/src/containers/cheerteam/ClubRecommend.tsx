6bd11f087930b9caa4c49f69c6c1cffdb7c8bb09
김미량 <miryang@naver.com>
2024-11-05 12:52:43 +0900

    feat: 추천 알고리즘 api 연결 테스트

--- frontend/src/containers/cheerteam/ClubRecommend.tsx
+++ frontend/src/containers/cheerteam/ClubRecommend.tsx
@@ -7,8 +7,38 @@ const ClubRecommend = () => {
   const [page, setPage] = useState<number>(0);
   const [selectedChoices, setSelectedChoices] = useState<number[]>([]);
   const [mbti, setMbti] = useState<string | null>(null);
-  const [region, setRegion] = useState<string>("충청");
+  const [region, setRegion] = useState<string>('');
+  // TODO: 유저 지역 정보 가져오기
   const [addPage, setAddPage] = useState<number>(1);
+
+  // 추천 알고리즘 연결
+  const ClubRecommendAPI = async () => {
+    const UserData = {
+      mbti: mbti,
+      responses: selectedChoices,
+      region: region,
+    };
+  
+    try {
+      // TODO: 실제 API 주소 입력하기
+      const response = await fetch("http://localhost:8000/recommend/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(UserData), // JSON 형태로 데이터 변환
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      console.log("Recommended team:", data.recommended_team);
+    } catch (error) {
+      console.error("Error sending data to API:", error);
+    }
+  };
 
   const handleMbtiSubmit = (writeMbti: string) => {
     setMbti(writeMbti);
@@ -27,12 +57,13 @@ const ClubRecommend = () => {
 
     // 최종 결과 출력
     if (page === questions.length) {
-      const choice = selectedChoice == 1 ? '' : region;
+      const choice = selectedChoice == 1 ? '' : '충청';
       setRegion(choice);
+      ClubRecommendAPI();
 
       console.log("MBTI:", mbti);
       console.log("모든 선택지:", selectedChoices);
-      console.log("지역:", choice)
+      console.log("지역:", region)
     }
 
     if (page < questions.length) {
