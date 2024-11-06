440294be6d00a541f6f34ddc01d0e1b9a1e8424e
김미량 <miryang@naver.com>
2024-11-06 17:53:06 +0900

    feat: 나의 직관 경기 기능 구현

--- frontend/src/containers/mypage/CameraCapture.tsx
+++ frontend/src/containers/mypage/CameraCapture.tsx
@@ -1,18 +1,70 @@
+import axios from 'axios';
 import { useState } from 'react';
 import Container from '../../components/common/Container';
 import CameraCaptureComponent from '../../components/mypage/CameraCapture';
 import ModalForm from './ModalForm';
 
+// base64 문자열을 Blob으로 변환하는 함수
+const base64ToBlob = (base64: string, mimeType: string): Blob => {
+  const byteCharacters = atob(base64.split(',')[1]);
+  const byteArrays = [];
+
+  // 각 문자를 byte로 변환하여 byteArrays 배열에 추가
+  for (let offset = 0; offset < byteCharacters.length; offset++) {
+    byteArrays.push(byteCharacters.charCodeAt(offset));
+  }
+
+  // Blob으로 변환하여 반환
+  return new Blob([new Uint8Array(byteArrays)], { type: mimeType });
+};
+
+// base64 문자열을 File 객체로 변환하는 함수
+const base64ToFile = (base64: string, fileName: string): File => {
+  const blob = base64ToBlob(base64, 'image/png');
+  return new File([blob], fileName, { type: 'image/png' });
+};
+
 const CameraCapture = () => {
   const [capturedImage, setCapturedImage] = useState<string>('');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [ocrResult, setOcrResult] = useState<string>('');
 
   // TODO: 캡쳐된 사진 보내기, OCR 분석 결과 받아오기
 
-  const handleCapture = (imageData: string) => {
+  const handleCapture = async (imageData: string) => {
     setCapturedImage(imageData);
     setIsModalOpen(true);
+  
+    const api_url = import.meta.env.VITE_TICKET_OCR
+
+    const userId = "1016";
+    const file = base64ToFile(imageData, 'image.png')
+
+    console.log(file)
+
+    // FormData 객체 생성
+    const formData = new FormData();
+    formData.append('userId', userId)
+    formData.append('image', file);
+  
+    try {
+      const response = await axios.post(api_url, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log('OCR 처리 결과:', response.data);
+      setOcrResult(response.data)
+      return response.data;
+    } catch (error) {
+      if (error) {
+        console.error('서버 오류:', error);
+      } else {
+        console.error('요청 실패:', error);
+      }
+    }
   };
+  
 
   const closeModal = () => {
     setIsModalOpen(false)
@@ -23,7 +75,7 @@ const CameraCapture = () => {
       <Container>
         <CameraCaptureComponent onCapture={handleCapture} />
         <div>
-          <ModalForm imgURL={capturedImage} isOpen={isModalOpen} onClose={closeModal} />
+          <ModalForm imgURL={capturedImage} isOpen={isModalOpen} onClose={closeModal} ocrResult={ocrResult} />
         </div>
       </Container>
     </div>
--- frontend/src/containers/mypage/ModalForm.tsx
+++ frontend/src/containers/mypage/ModalForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react' 
 import ModalFormComponent from "../../components/mypage/ModalForm";
 import ModalImage from "../../components/mypage/ModalImage";
 import ModalInput from "../../components/mypage/ModalInput";
@@ -9,30 +10,68 @@ interface ModalFormProps {
   imgURL: string;
   isOpen: boolean;
   onClose: () => void; // 모달 닫기 함수
+  ocrResult: string;
 }
 
 const ModalForm = (props: ModalFormProps) => {
   if (!props.isOpen) return null;
 
+  const [memoText, setMemoText] = useState<string>("");
+
+  const handleMemoChange = (newText: string) => {
+    setMemoText(newText);
+  };
+
+  console.log(props.ocrResult); // 일단 그냥 써둠
+
+  const TextSaveAPI = async () => {
+
+    const api_url = import.meta.env.VITE_TICKET_TEXT
+
+    const TextData = {
+      id: "672b18692d22ab456ed56763",
+      text: memoText
+    };
+
+    try {
+      const response = await fetch(api_url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(TextData), // JSON 형태로 데이터 변환
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error sending data to API:", error);
+    }
+  };
+
   const onSaveClick = () => {
     console.log("저장하기 버튼 클릭");
+    TextSaveAPI();
   };
 
   const onReClick = () => {
-    console.log("다시 찍기 버튼 클릭");
     props.onClose();
   };
 
   return (
     <>
-    <Container>
-      <div
-        className="fixed bottom-0 left-0 right-0 flex flex-col items-center bg-gray-100 w-full h-full shadow-lg"
-        style={{ zIndex: 50 }}
-      >
+      <Container>
+        <div
+          className="fixed bottom-0 left-0 right-0 flex flex-col items-center bg-gray-100 w-full h-full shadow-lg"
+          style={{ zIndex: 50 }}
+        >
           <ModalFormComponent date="2024.08.03" team1="키움 히어로즈" team2="두산 베어스" />
           <ModalImage imageUrl={props.imgURL} />
-          <ModalInput />
+          <ModalInput onMemoChange={handleMemoChange} />
           <div className="flex flex-row mb-10">
             <Button
               className="w-1/2 mr-4 flex justify-center"
@@ -47,8 +86,8 @@ const ModalForm = (props: ModalFormProps) => {
               onClick={onReClick}
             />
           </div>
-    </div>
-    </Container>
+        </div>
+      </Container>
     </>
   );
 };
