1fd26e144050c02989ddea62343cd800c9c9c1ee
poow810 <poow810@naver.com>
2024-11-06 13:14:23 +0900

    [BE] fext : swagger description 추가

--- backend/mypage/src/main/java/com/newbie/mypage/watch_game/controller/TicketController.java
+++ backend/mypage/src/main/java/com/newbie/mypage/watch_game/controller/TicketController.java
@@ -4,9 +4,12 @@ import com.newbie.mypage.watch_game.dto.TicketResponseDto;
 import com.newbie.mypage.watch_game.dto.TicketTextDto;
 import com.newbie.mypage.watch_game.service.OcrService;
 import com.newbie.mypage.watch_game.service.TicketService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,8 +35,9 @@ public class TicketController {
      * @param userId
      * @return 경기 날짜, 경기 팀1, 경기 팀2
      */
+    @Operation(summary = "티켓 OCR 분석", description = "사용자가 티켓 이미지 등록 시 OCR 분석을 실시합니다.")
     @PostMapping(value = "/naverOcr", consumes = "multipart/form-data")
-    public ResponseEntity<?> ocr(@RequestParam("image") MultipartFile image, @RequestParam int userId) {
+    public ResponseEntity<?> ocr(@RequestParam("image") @Parameter(description = "이미지") MultipartFile image , @RequestParam int userId) {
         try {
             Map<String, Object> result = ocrService.processAndSaveTicket(image, userId);
             return ResponseEntity.ok(result);
@@ -50,6 +54,7 @@ public class TicketController {
      * @param userId
      * @return 유저의 직관 경기 리스트
      */
+    @Operation(summary = "직관 경기 리스트 확인", description = "사용자가 직관간 경기의 리스트를 확인합니다.")
     @GetMapping("/list")
     public ResponseEntity<List<TicketResponseDto>> getTicketList(@RequestParam int userId) {
         List<TicketResponseDto> result = ticketService.getTicketList(userId);
@@ -64,8 +69,9 @@ public class TicketController {
      * @param id
      * @return
      */
+    @Operation(summary = "직관 경기 확인", description = "사용자가 직관간 경기를 확인합니다.")
     @GetMapping()
-    public ResponseEntity<TicketResponseDto> getTicket(@RequestParam String id) {
+    public ResponseEntity<TicketResponseDto> getTicket(@RequestParam @Parameter(description = "티켓 ID") String id) {
         TicketResponseDto result = ticketService.getTicket(id);
         if (result == null) {
             return ResponseEntity.notFound().build();
@@ -73,14 +79,16 @@ public class TicketController {
         return ResponseEntity.ok(result);
     }
 
+    @Operation(summary = "직관 경기 삭제", description = "사용자가 직관간 경기를 삭제합니다.")
     @DeleteMapping("/delete")
-    public ResponseEntity<?> deleteTicket(@RequestParam String id) {
+    public ResponseEntity<?> deleteTicket(@RequestParam @Parameter(description = "티켓 ID") String id) {
         ticketService.deleteTicket(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "직관 경기 text 추가", description = "사용자가 직관 경기에 text를 추가합니다..")
     @PutMapping("/text")
-    public ResponseEntity<Void> updateText(@RequestBody TicketTextDto textDto) {
+    public ResponseEntity<Void> updateText(@RequestBody @Parameter(description = "티켓 ID와 Text") TicketTextDto textDto) {
         ticketService.updateText(textDto.getId(), textDto.getText());
         return ResponseEntity.status(HttpStatus.OK).build();
     }
