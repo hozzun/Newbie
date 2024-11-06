3ad32c6bfffce4db44f83ecc769c9f1c9b226e0e
poow810 <poow810@naver.com>
2024-11-06 14:24:04 +0900

    [BE] fix : webConfig 설정 변경

--- backend/mypage/src/main/java/com/newbie/mypage/config/WebConfig.java
+++ backend/mypage/src/main/java/com/newbie/mypage/config/WebConfig.java
@@ -1,16 +1,29 @@
 package com.newbie.mypage.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
-                .allowedMethods("GET", "POST") // 허용할 HTTP method
-                .allowCredentials(true); // 쿠키 인증 요청 허용
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://k11b304.p.ssafy.io")); // 허용할 출처 설정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 설정
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 자격 증명 허용 (JWT 포함)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
+
 }
