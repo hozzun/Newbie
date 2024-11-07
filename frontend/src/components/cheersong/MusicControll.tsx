62439f1a3c26c6b095dd42da79f660476cd44969
김미량 <miryang@naver.com>
2024-11-07 11:10:12 +0900

    feat: 응원가 상세조회 페이지 생성

--- frontend/src/components/cheersong/MusicControll.tsx
+++ frontend/src/components/cheersong/MusicControll.tsx
@@ -1,14 +1,20 @@
 import RewindSolid from '../../assets/icons/rewind-solid.svg?react'
 import Pause from '../../assets/icons/pause-solid.svg?react'
 
-const MusicControll = () => {
+interface MusicControllProps {
+  onClickLeft: () => void;
+  onClickPause: () => void;
+  onClickRight: () => void;
+}
+
+const MusicControll = (props: MusicControllProps) => {
 
   return (
-    <div className='fixed bottom-10 flex flex-row justify-center items-center text-gray-400'>
-      <RewindSolid className='w-5 h-5 mx-10'/>
-      <Pause className='w-5 h-5 mx-10' />
-      <RewindSolid className='w-5 h-5 mx-10 rotate-180' />
-    </div>
+      <div className='fixed bottom-10 flex flex-row justify-center items-center text-gray-400 ml-24'>
+        <RewindSolid className='w-5 h-5 mx-10' onClick={props.onClickLeft} />
+        <Pause className='w-5 h-5 mx-10' onClick={props.onClickPause} />
+        <RewindSolid className='w-5 h-5 mx-10 rotate-180' onClick={props.onClickRight} />
+      </div>
   )
 }
 
--- frontend/src/pages/cheersong/CheerLyris.tsx
+++ frontend/src/pages/cheersong/CheerLyris.tsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Container from "../../components/common/Container"
 import SectionBox from "../../components/common/SectionBox"
 import CheerLyrisContainer from "../../containers/cheersong/CheerLyris"
 
 const CheerLyris = () => {
 
+  const navigate = useNavigate();
+  
+  const navHandle = () => {
+    navigate("/cheersong") 
+  }
+
   return (
     <>
-      <SectionBox label="응원가" />
+      <SectionBox label="응원가" onClick={navHandle} />
       <Container>
         <CheerLyrisContainer />
       </Container>
