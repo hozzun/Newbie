d0edcc0e9f39bf6ab7ae979ff5f131773a599d21
김미량 <miryang@naver.com>
2024-11-07 11:24:19 +0900

    feat: 응원가 상세조회 페이지 구현 완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import AppRouter from "./AppRouter";
-import CheerLyris from "./pages/cheersong/CheerLyris";
 
 function App() {
   return (
     <>
-      {/* <AppRouter /> */}
-      <CheerLyris />
+      <AppRouter />
     </>
   );
 }
--- frontend/src/pages/cheersong/CheerLyris.tsx
+++ frontend/src/pages/cheersong/CheerLyris.tsx
@@ -1,19 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import Container from "../../components/common/Container"
 import SectionBox from "../../components/common/SectionBox"
 import CheerLyrisContainer from "../../containers/cheersong/CheerLyris"
 
 const CheerLyris = () => {
 
-  const navigate = useNavigate();
-  
-  const navHandle = () => {
-    navigate("/cheersong") 
-  }
-
   return (
     <>
-      <SectionBox label="응원가" onClick={navHandle} />
+      <SectionBox label="응원가" />
       <Container>
         <CheerLyrisContainer />
       </Container>
