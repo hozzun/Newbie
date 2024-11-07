61583c8725e795ad7778afef12662917cbf5e70d
김미량 <miryang@naver.com>
2024-11-07 10:36:18 +0900

    feat: music controll 버튼 컴포넌트 생성

--- frontend/src/components/cheersong/MusicControll.tsx
+++ frontend/src/components/cheersong/MusicControll.tsx
@@ -1,7 +1,14 @@
+import RewindSolid from '../../assets/icons/rewind-solid.svg?react'
+import Pause from '../../assets/icons/pause-solid.svg?react'
+
 const MusicControll = () => {
 
   return (
-    <div></div>
+    <div className='fixed bottom-10 flex flex-row justify-center items-center text-gray-400'>
+      <RewindSolid className='w-5 h-5 mx-10'/>
+      <Pause className='w-5 h-5 mx-10' />
+      <RewindSolid className='w-5 h-5 mx-10 rotate-180' />
+    </div>
   )
 }
 
--- frontend/src/containers/cheersong/CheerLyris.tsx
+++ frontend/src/containers/cheersong/CheerLyris.tsx
@@ -1,5 +1,6 @@
 import CheerSong from "../../components/cheersong/CheerSong"
 import CheerLyrisComponent from "../../components/cheersong/CheerLyris"
+import MusicControll from "../../components/cheersong/MusicControll"
 
 const CheerLyris = () => {
 
@@ -8,6 +9,7 @@ const CheerLyris = () => {
       <div className="m-5">
         <CheerSong club="ssg" title="랜더스여" singer="We are 랜더스 파이팅" showIcon={false} />
         <CheerLyrisComponent lyris="가사" />
+        <MusicControll />
       </div>
     </>
   )
