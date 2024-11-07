87cdd894ad416e7fbdfbe38929b82860c22124de
이지원 <wldnjs7851@naver.com>
2024-11-07 12:35:18 +0900

    feat: 선수단 목록 정렬 컴포넌트 구현
    - 정렬 셀렉트 공통 컴포넌트 구현

---
+++ frontend/src/components/common/SortSelectBox.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import CaretDownSolid from "../../assets/icons/caret-down-solid.svg?react";
+import CaretUpSolid from "../../assets/icons/caret-up-solid.svg?react";
+import CircleButton from "./CircleButton";
+import { CIRCLE_BUTTON_VARIANTS } from "./variants";
+
+interface DropdownProps {
+  options: Array<string>;
+  selected: string;
+  onSelect: (value: string) => void;
+}
+
+interface SortSelectBoxProps {
+  options: Array<string>;
+}
+
+const Dropdown = (props: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => setIsOpen(!isOpen);
+  const handleSelect = (value: string) => {
+    props.onSelect(value);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="relative inline-block">
+      <div className="flex flex-row">
+        <p className="pr-1 py-2 text-sm font-kbogothiclight text-gray-700">
+          {props.selected || props.options[0]}
+        </p>
+        {isOpen ? (
+          <CircleButton
+            className="w-10 h-10"
+            variant={CIRCLE_BUTTON_VARIANTS.grayLine}
+            item={{ img: CaretUpSolid }}
+            onClick={handleToggle}
+          />
+        ) : (
+          <CircleButton
+            className="w-10 h-10"
+            variant={CIRCLE_BUTTON_VARIANTS.grayLine}
+            item={{ img: CaretDownSolid }}
+            onClick={handleToggle}
+          />
+        )}
+      </div>
+      {isOpen && (
+        <div className="absolute right-2 z-10 min-w-[90px] mt-0.5 bg-white border border-gray-200 rounded-lg shadow-xl">
+          {props.options.map((option, index) => (
+            <div
+              key={index}
+              onClick={() => handleSelect(option)}
+              className="w-full px-4 py-2 hover:bg-gray-100 hover:rounded-lg cursor-pointer text-sm font-kbogothiclight text-gray-700"
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const SortSelectBox = (props: SortSelectBoxProps) => {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelect = (value: string) => setSelectedOption(value);
+
+  return <Dropdown options={props.options} selected={selectedOption} onSelect={handleSelect} />;
+};
+
+export default SortSelectBox;
--- frontend/src/components/player/PlayerList.tsx
+++ frontend/src/components/player/PlayerList.tsx
@@ -1,30 +1,38 @@
 import PlayerItem from "../common/PlayerItem";
+import SortSelectBox from "../common/SortSelectBox";
+
+const options: Array<string> = ["이름순", "등번호순", "좋아요순"];
 
 const PlayerList = () => {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
-      <PlayerItem />
+    <div className="flex flex-col w-full">
+      <div className="flex justify-end">
+        <SortSelectBox options={options} />
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+        <PlayerItem />
+      </div>
     </div>
   );
 };
