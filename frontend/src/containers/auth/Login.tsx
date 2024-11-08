d876ff0beb019d86c04c398481d0a5cc42c1ea57
jinwoo <yasisicoco@gmail.com>
2024-11-08 11:21:51 +0900

    feat: 로그인 모달 로직 수정

--- frontend/src/containers/auth/Login.tsx
+++ frontend/src/containers/auth/Login.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LoginComponent from "../../components/auth/Login";
 import { KAKAO_AUTH_URL, GOOGLE_AUTH_URL } from "../../api/Oauth";
 import AuthorityBottomSheet from "../../components/common/AuthorityBottomSheet";
 
 const Login = () => {
   const [showBottomSheet, setShowBottomSheet] = useState(true);
+
+  useEffect(() => {
+    const permission = Notification.permission;
+    if (permission === "default") {
+      setShowBottomSheet(true);
+    } else {
+      setShowBottomSheet(false);
+    }
+  }, []);
 
   const handleKakaoLoginClick = () => {
     window.location.href = KAKAO_AUTH_URL;
