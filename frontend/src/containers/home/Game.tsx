fe57e4ecbdef2ebb7ed9f6e4b7a50e56cffe6b0d
이지원 <wldnjs7851@naver.com>
2024-11-11 14:53:54 +0900

    feat: 경기 결과 페이지 이동
    - 경기 일정에서 종료된 경기인 경우 클릭 시 경기 결과 페이지로 이동

--- frontend/src/containers/home/Game.tsx
+++ frontend/src/containers/home/Game.tsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import GameComponent from "../../components/home/Game";
 import { GameProps } from "./Home";
 
 const Game = (props: GameProps) => {
+  const nav = useNavigate();
+
   const today = new Date().toISOString().split("T")[0];
 
   const goDetail = () => {
-    // TODO: MOVE - 경기 결과 페이지
-    console.log("경기 결과 페이지 이동");
+    nav(`/game/result/${props.gameInfo.id}`);
   };
 
   return props.gameInfo.day < today || props.gameSituation.isPlaying ? (
