f8fa93af14ed4bd869c0cc816e5a79e6b1a5e322
김미량 <miryang@naver.com>
2024-11-11 14:47:52 +0900

    refactor: 응원 구단 수정 API 수정

--- frontend/src/containers/cheerteam/RecommendResult.tsx
+++ frontend/src/containers/cheerteam/RecommendResult.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axiosInstance from "../../util/axiosInstance";
 import { useNavigate } from "react-router-dom";
 import RecommendResultComponent from "../../components/cheerteam/RecommendResult"
 import ResultButton from "../../components/cheerteam/ResultButton"
@@ -23,14 +23,11 @@ const RecommendResult = (props: RecommendResultProps) => {
 
   const nav = useNavigate()
 
-  // TODO: URL 수정
   const updateFavoriteTeam = async (favoriteTeamId: number) => {
 
-    const api_url = import.meta.env.VITE_CHEER_TEAM
-
     try {
-      const response = await axios.patch(api_url, {
-        favoriteTeamId: favoriteTeamId,
+      const response = await axiosInstance.patch("/api-user/users/favorite-team", {
+        teamId: favoriteTeamId,
       });
   
       console.log("응답 결과:", response.data);
