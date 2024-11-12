64b5b760c708c75aa3e13b421f47dcea21437e91
gawon <gawon_127@naver.com>
2024-11-12 16:09:46 +0900

    feat: gateway 라우팅 설정

--- backend/gateway/src/main/java/newbie/gateway/gateway/config/SecurityConfig.java
+++ backend/gateway/src/main/java/newbie/gateway/gateway/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())  // CSRF 비활성화
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/", "/api-auth/**").permitAll() // 인증 없이 접근 가능 경로
+                        .pathMatchers("/", "/api/v1/login/**", "/api/v1/members/**").permitAll() // 인증 없이 접근 가능 경로
                         .anyExchange().authenticated()  // 그 외 경로는 인증 필요
                 );
 
