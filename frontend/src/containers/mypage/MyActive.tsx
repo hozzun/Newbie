074fc523386e38ed153bf28c45f7f3df85b8c088
김미량 <miryang@naver.com>
2024-11-14 11:24:48 +0900

    feat: 마이페이지 나의 활동 API 연결

--- frontend/src/containers/mypage/MyActive.tsx
+++ frontend/src/containers/mypage/MyActive.tsx
@@ -1,53 +1,63 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axiosInstance from '../../util/axiosInstance';
 import MyActiveLike from "../../components/mypage/MyActiveLike";
 import MyActiveComment from "../../components/mypage/MyActiveComment";
 
 interface LikeData {
-  time: string;
+  boardId: number;
+  createdAt: string;
   title: string;
-  onClick: () => void;
+  onClick: () => void; // TODO: 클릭하면 삭제하는 API 연결
 }
 
 interface CommentData {
-  comment: string;
-  time: string;
+  boardId: number;
+  content: string;
+  createdAt: string;
   onClick: () => void;
 }
 
 const MyActive = () => {
-  const [likes, setLikes] = useState<LikeData[]>([
-    {
-      title: "기본 게시물 제목",
-      time: new Date().toLocaleDateString(),
-      onClick: () => console.log('삭제')
-    },
-  ]);
-  const [comments, setComments] = useState<CommentData[]>([
-    {
-      comment: "기본 댓글",
-      time: new Date().toLocaleDateString(),
-      onClick: () => console.log('삭제')
-    },
-  ]);
+  const [likes, setLikes] = useState<LikeData[]>([]);
+  const [comments, setComments] = useState<CommentData[]>([]);
+
+  const getActivity = async () => {
+
+    const userId = 5
+    const params = { userId: userId }
   
-  console.log(setLikes);
-  console.log(setComments);
+    try {
+      const response = await axiosInstance.get(`/api-board/user-activity/${userId}`, { params });
+      if (response.data.type === 'like') {
+        setLikes(response.data)
+      } else if (response.data.type === 'comment') {
+        setComments(response.data)
+      }
+      
+    } catch (error) {
+      console.error("에러 발생:", error);
+    }
+  };
+  
+    useEffect(() => {
+      getActivity();
+    }, []);
 
   return (
     <>
-      {likes.map((like, index) => (
+      {likes.map((like) => (
         <MyActiveLike
-          key={index}
-          time={like.time}
+          key={like.boardId}
+          time={like.createdAt}
           title={like.title}
           onClick={like.onClick}
         />
       ))}
-      {comments.map((comment, index) => (
+      {comments.map((comment) => (
         <MyActiveComment
-          key={index}
-          time={comment.time}
-          comment={comment.comment}
+          key={comment.boardId}
+          time={comment.createdAt}
+          comment={comment.content}
           onClick={comment.onClick}
         />
       ))}
