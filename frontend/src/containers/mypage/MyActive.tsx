ce4bd69eedc876b5a6249e42ca5dff4eb0dfd23e
김미량 <miryang@naver.com>
2024-11-15 10:09:02 +0900

    feat: 나의 활동 삭제 API 연결

--- frontend/src/containers/mypage/MyActive.tsx
+++ frontend/src/containers/mypage/MyActive.tsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
-import axiosInstance from '../../util/axiosInstance';
+import { useState, useEffect } from "react";
+import axiosInstance from "../../util/axiosInstance";
 import MyActiveLike from "../../components/mypage/MyActiveLike";
 import MyActiveComment from "../../components/mypage/MyActiveComment";
 
 interface LikeData {
-  type: 'like'
+  type: "like";
   boardId: number;
   createdAt: string;
   title: string;
-  onClick: () => void; // TODO: 클릭하면 삭제하는 API 연결
+  onClick: () => void;
 }
 
 interface CommentData {
-  type: 'comment'
+  type: "comment";
   boardId: number;
   content: string;
   createdAt: string;
@@ -25,50 +25,71 @@ const MyActive = () => {
   const [activities, setActivities] = useState<ActivityData[]>([]);
 
   const getActivity = async () => {
-
     // TODO: userId 수정
-    const userId = 5
-    const params = { userId: userId }
-  
+    const userId = 5;
+    const params = { userId: userId };
+
     try {
       const response = await axiosInstance.get(`/api-board/user-activity/${userId}`, { params });
-      const likesData: LikeData[] = response.data.filter((item: { type: string }) => item.type === 'like')
-      .map((like: Omit<LikeData, 'type'>) => ({
-        ...like,
-        type: 'like',
-      }));
-    
-    const commentsData: CommentData[] = response.data.filter((item: { type: string }) => item.type === 'comment')
-      .map((comment: Omit<CommentData, 'type'>) => ({
-        ...comment,
-        type: 'comment',
-      }));
+      const likesData: LikeData[] = response.data
+        .filter((item: { type: string }) => item.type === "like")
+        .map((like: Omit<LikeData, "type">) => ({
+          ...like,
+          type: "like",
+        }));
 
-      // 병합 후 createdAt 기준으로 내림차순 정렬
+      const commentsData: CommentData[] = response.data
+        .filter((item: { type: string }) => item.type === "comment")
+        .map((comment: Omit<CommentData, "type">) => ({
+          ...comment,
+          type: "comment",
+        }));
+
+      // createdAt 기준으로 내림차순 정렬
       const combinedData: ActivityData[] = [...likesData, ...commentsData].sort(
-        (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
       );
 
       setActivities(combinedData);
-      
     } catch (error) {
       console.error("에러 발생:", error);
     }
   };
-  
-    useEffect(() => {
-      getActivity();
-    }, []);
 
-    return (
-      <>
-        {activities.length > 0 ? (activities.map((activity) =>
-          activity.type === 'like' ? (
+  const deleteActivity = async () => {
+    // TODO: userId 수정, activityId가 뭔데 ..
+    const userId = 5;
+    const activityId = 1;
+    const params = { userId: userId, activityId: activityId };
+
+    try {
+      const response = await axiosInstance.delete(`/api-board/user-activity/${activityId}`, {
+        params,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error("에러 발생:", error);
+    }
+  };
+
+  const deleteAct = () => {
+    deleteActivity();
+  };
+
+  useEffect(() => {
+    getActivity();
+  }, []);
+
+  return (
+    <>
+      {activities.length > 0 ? (
+        activities.map(activity =>
+          activity.type === "like" ? (
             <div key={activity.boardId} onClick={() => console.log("해당 좋아요 페이지로 이동")}>
               <MyActiveLike
                 time={activity.createdAt}
                 title={(activity as LikeData).title}
-                onClick={activity.onClick}
+                onClick={deleteAct}
               />
             </div>
           ) : (
@@ -76,14 +97,18 @@ const MyActive = () => {
               <MyActiveComment
                 time={activity.createdAt}
                 comment={(activity as CommentData).content}
-                onClick={activity.onClick}
+                onClick={deleteAct}
               />
             </div>
-          )
-        )):
-        <p className='font-kbogothicmedium flex justify-center items-center text-gray-600'>아직 활동한 기록이 없습니다.</p>}
-      </>
-    );
+          ),
+        )
+      ) : (
+        <p className="font-kbogothicmedium flex justify-center items-center text-gray-600">
+          아직 활동한 기록이 없습니다.
+        </p>
+      )}
+    </>
+  );
 };
 
 export default MyActive;
