8e01650e0f2bc0d8725c9b896c9321f6b34cbd82
hojun <didl95@naver.com>
2024-11-15 15:12:37 +0900

    feat: 응원송 가사 추가 및 redis 적용

--- backend/baseball/src/main/java/com/newbie/baseball/global/config/RedisConfig.java
+++ backend/baseball/src/main/java/com/newbie/baseball/global/config/RedisConfig.java
@@ -1,14 +1,23 @@
 package com.newbie.baseball.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableCaching
 @Configuration
@@ -35,5 +44,31 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // gamesCache: 6시간 TTL
+        RedisCacheConfiguration gamesCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(6));
+
+//        // teamSongsCache: 15시간 TTL
+//        RedisCacheConfiguration teamSongsCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+//                .entryTtl(Duration.ofHours(15));
+//
+//        // playerSongsCache: 15시간 TTL
+//        RedisCacheConfiguration playerSongsCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+//                .entryTtl(Duration.ofHours(15));
+
+        // 각 캐시에 대해 설정 매핑
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+        cacheConfigurations.put("gamesCache", gamesCacheConfig);
+//        cacheConfigurations.put("teamSongsCache", teamSongsCacheConfig);
+//        cacheConfigurations.put("playerSongsCache", playerSongsCacheConfig);
+
+        // RedisCacheManager 빌더에 캐시 설정 적용
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .withInitialCacheConfigurations(cacheConfigurations) // 각 캐시별 설정
+                .build(); // 기본 캐시 TTL은 설정하지 않음
     }
 }
