2bf9c27684796b80c151f6558657451e46b7f328
jinwoo <yasisicoco@gmail.com>
2024-11-17 00:45:38 +0900

    feat: 인트로 수정 및 캐러셀 바꿈

--- frontend/src/components/intro/IntroSlideItem.tsx
+++ frontend/src/components/intro/IntroSlideItem.tsx
@@ -6,11 +6,11 @@ interface SlideItemProps {
 
 const IntroSlideItem = ({ imgSrc, text1, text2 }: SlideItemProps) => {
   return (
-    <div className="flex flex-col items-center justify-center h-full space-y-8 text-center px-8 my-[10%]">
+    <div className="flex flex-col items-center justify-center h-full space-y-12 text-center px-8 my-[10%]">
       <img
         src={imgSrc}
         alt={text1}
-        className="w-full max-w-[80%] h-auto object-contain rounded-lg"
+        className="w-full max-w-[90%] h-auto object-contain rounded-lg"
       />
       <div className="text-4xl font-kbogothicbold mt-[10%]">{text1}</div>
       <div className="text-2lg text-gray-500 font-kbogothicmedium mb-[30%]">{text2}</div>{" "}
