30e50fb64ef2b8d3cbfb112455a4663ae712598f
poow810 <poow810@naver.com>
2024-11-19 01:52:55 +0900

    [BE] fix : config 설정 변경

--- backend/gateway/src/main/java/newbie/gateway/gateway/config/CorsConfig.java
+++ backend/gateway/src/main/java/newbie/gateway/gateway/config/CorsConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
-import java.util.List;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -13,37 +14,14 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-
-        // 허용할 도메인
-        config.setAllowedOriginPatterns(List.of(
+        config.setAllowedOriginPatterns(Arrays.asList(
                 "http://localhost:5173",
                 "https://k11b304.p.ssafy.io"
         ));
-
-        // 허용할 HTTP 헤더
-        config.setAllowedHeaders(List.of(
-                "Authorization",
-                "Content-Type",
-                "Accept",
-                "Origin",
-                "X-Requested-With"
-        ));
-
         config.addAllowedHeader("*");
-
-        // 허용할 HTTP 메서드
-        config.setAllowedMethods(List.of(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "OPTIONS"
-        ));
-
-        // 자격 증명 허용
+        config.addAllowedMethod("*");
         config.setAllowCredentials(true);
 
-        // 설정 적용
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
